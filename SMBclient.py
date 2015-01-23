5805dc6a502290b3085ff0838934bd9d8aef49d9
humberry <brummberry@gmail.com>
2015-01-23 23:34:20 +0100

    create ...

---
+++ SMBclient.py
@@ -0,0 +1,334 @@
+# You need the impacket python module (http://corelabs.coresecurity.com)
+# Extract the impacket directory to site-packages (https://pypi.python.org/packages/source/i/impacket/)
+
+import ui, os
+import sys
+import string
+import time
+from impacket import smb, version, smb3, nt_errors
+from impacket.dcerpc.v5 import samr, transport, srvs
+from impacket.dcerpc.v5.dtypes import NULL
+from impacket.smbconnection import *
+
+class SMBclient(ui.View):
+
+  def __init__(self):
+    self.fileName = ''
+    self.localFile = ''
+    self.remoteFile = ''
+    self.smb = None
+    self.pwd = ''
+    self.share = None
+    self.loggedIn = False
+    self.password = None
+    self.username = None
+    self.host = None 
+    self.root = os.path.expanduser('~')
+    self.path = os.getcwd()
+    self.view = ui.load_view('SMBclient')
+    self.view['bt_connect'].action = self.bt_connect
+    self.view['bt_upload'].action = self.bt_upload
+    self.view['bt_download'].action = self.bt_download
+    self.view['bt_local_rename'].action = self.bt_local_rename
+    self.view['bt_local_delete'].action = self.bt_local_delete
+    self.view['bt_local_mkdir'].action = self.bt_local_mkdir
+    self.view['bt_remote_rename'].action = self.bt_remote_rename
+    self.view['bt_remote_delete'].action = self.bt_remote_delete
+    self.view['bt_remote_mkdir'].action = self.bt_remote_mkdir
+    root_len = len(self.root)
+    self.view['lb_local'].text = self.path[root_len:]
+    self.tv_local = self.view['tv_local']
+    self.tv_remote = self.view['tv_remote']
+    self.tv_info = self.view['tv_info']
+    all = self.get_dir()
+    self.lst_local = ui.ListDataSource(all)
+    self.tv_local.data_source = self.lst_local
+    self.tv_local.delegate = self.lst_local
+    self.tv_local.editing = False
+    self.lst_local.font = ('Courier',14)
+    self.lst_local.action = self.table_local_tapped
+    self.lst_local.delete_enabled = False 
+    self.lst_remote = ui.ListDataSource([''])
+    self.tv_remote.data_source = self.lst_remote
+    self.tv_remote.delegate = self.lst_remote
+    self.tv_remote.editing = False
+    self.lst_remote.font = ('Courier',14)
+    self.lst_remote.action = self.table_remote_tapped
+    self.lst_remote.delete_enabled = False 
+    self.view.present('fullscreen')
+
+  def bt_local_rename(self, sender):
+    pos = self.localFile.rfind('/')
+    self.fileName = self.localFile[pos+1:]
+    self.view_po = ui.load_view('popover')
+    self.view_po.name = 'Rename'
+    self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+    self.view_po['lb_old_name'].text = self.fileName
+    self.view_po['tf_new_name'].text = self.fileName
+    self.view_po['bt_okay'].action = self.bt_local_rename_okay
+    self.view_po['bt_cancel'].action = self.bt_cancel
+
+  def bt_local_rename_okay(self, sender):
+    os.rename(self.fileName, self.view_po['tf_new_name'].text)
+    self.view_po.close()
+    all = self.get_dir()
+    self.refresh_table(self.tv_local,self.lst_local,all)
+
+  def bt_cancel(self, sender):
+    self.view_po.close()
+
+  def bt_local_delete(self, sender):
+    pos = self.localFile.rfind('/')
+    self.fileName = self.localFile[pos+1:]
+    self.view_po = ui.load_view('popover')
+    self.view_po.name = 'Delete'
+    self.view_po['tf_new_name'].hidden = True 
+    self.view_po['lb_nn'].hidden = True
+    self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+    self.view_po['lb_on'].text = 'Delete:'
+    self.view_po['lb_old_name'].text = self.fileName
+    self.view_po['bt_cancel'].action = self.bt_cancel
+    self.view_po['bt_okay'].action = self.bt_local_delete_okay
+
+  def bt_local_delete_okay(self, sender):
+    os.remove(self.localFile)
+    self.view_po.close()
+    all = self.get_dir()
+    self.refresh_table(self.tv_local,self.lst_local,all)
+
+  def bt_local_mkdir(self, sender):
+    self.view_po = ui.load_view('popover')
+    self.view_po.name = 'Make Directory'
+    self.view_po['lb_old_name'].hidden = True 
+    self.view_po['lb_on'].hidden = True
+    self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+    self.view_po['lb_nn'].text = 'New Dir:'
+    self.view_po['bt_cancel'].action = self.bt_cancel
+    self.view_po['bt_okay'].action = self.bt_local_mkdir_okay
+
+  def bt_local_mkdir_okay(self, sender):
+    directory = self.view_po['tf_new_name'].text
+    os.mkdir(self.path + '/' + directory)
+    self.view_po.close()
+    all = self.get_dir()
+    self.refresh_table(self.tv_local,self.lst_local,all)
+
+  def bt_remote_rename(self, sender):
+    if self.loggedIn:
+      pos = self.remoteFile.rfind('\\')
+      self.fileName = self.remoteFile[pos+1:]
+      self.view_po = ui.load_view('popover')
+      self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+      self.view_po['lb_old_name'].text = self.fileName
+      self.view_po['tf_new_name'].text = self.fileName
+      self.view_po['bt_okay'].action = self.bt_remote_rename_okay
+      self.view_po['bt_cancel'].action = self.bt_cancel
+
+  def bt_remote_rename_okay(self, sender):
+    self.smb.rename(self.share, self.remoteFile, self.pwd + '\\' + self.view_po['tf_new_name'].text)
+    self.view_po.close()
+    all = self.get_remote_dir()
+    self.refresh_table(self.tv_remote,self.lst_remote,all)
+
+  def bt_remote_delete(self, sender):
+    if self.loggedIn:
+      pos = self.remoteFile.rfind('\\')
+      self.fileName = self.remoteFile[pos+1:]
+      self.view_po = ui.load_view('popover')
+      self.view_po.name = 'Delete'
+      self.view_po['tf_new_name'].hidden = True 
+      self.view_po['lb_nn'].hidden = True
+      self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+      self.view_po['lb_on'].text = 'Delete:'
+      self.view_po['lb_old_name'].text = self.fileName
+      self.view_po['bt_cancel'].action = self.bt_cancel
+      self.view_po['bt_okay'].action = self.bt_remote_delete_okay
+
+  def bt_remote_delete_okay(self, sender):
+    self.smb.deleteFile(self.share, self.remoteFile)
+    self.view_po.close()
+    all = self.get_remote_dir()
+    self.refresh_table(self.tv_remote,self.lst_remote,all)
+
+  def bt_remote_mkdir(self, sender):
+    if self.loggedIn:
+      self.view_po = ui.load_view('popover')
+      self.view_po.name = 'Make Directory'
+      self.view_po['lb_old_name'].hidden = True 
+      self.view_po['lb_on'].hidden = True
+      self.view_po.present('popover',popover_location=(self.view.width/2,self.view.height/2))
+      self.view_po['lb_nn'].text = 'New Dir:'
+      self.view_po['bt_cancel'].action = self.bt_cancel
+      self.view_po['bt_okay'].action = self.bt_remote_mkdir_okay
+
+  def bt_remote_mkdir_okay(self, sender):
+    directory = self.view_po['tf_new_name'].text
+    if self.pwd == '\\':
+      self.smb.createDirectory(self.share,self.pwd + directory)
+    else:
+      self.smb.createDirectory(self.share,self.pwd + '\\' + directory)
+    self.view_po.close()
+    all = self.get_remote_dir()
+    self.refresh_table(self.tv_remote,self.lst_remote,all)
+
+  def bt_connect(self, sender):
+    hostshare = self.view['tf_host'].text
+    if hostshare.find('/') > 0:
+      self.host, self.share = hostshare.split('/')
+    self.username = self.view['tf_user'].text
+    self.password = self.view['tf_password'].text
+    
+    if self.loggedIn is False:
+      try:
+        self.smb = SMBConnection('*SMBSERVER', self.host, sess_port=139)
+        dialect = self.smb.getDialect()
+        if dialect == SMB_DIALECT:
+          self.tv_info.text = "SMBv1 dialect used"
+        elif dialect == SMB2_DIALECT_002:
+          self.tv_info.text = "SMBv2.0 dialect used"
+        elif dialect == SMB2_DIALECT_21:
+          self.tv_info.text = "SMBv2.1 dialect used"
+        else:
+          self.tv_info.text = "SMBv3.0 dialect used"
+        if self.smb is None:
+          self.tv_info.text = "No connection open"
+          return
+        self.smb.login(self.username, self.password, domain='')
+        if self.smb.isGuestSession() > 0:
+          self.tv_info.text += "\nGUEST Session Granted"
+        else:
+          self.tv_info.text += "\nUSER Session Granted"
+        self.loggedIn = True
+        self.smb.connectTree(self.share)
+        self.pwd = '\\'
+        all = self.get_remote_dir()
+        self.refresh_table(self.tv_remote,self.lst_remote,all)
+        self.view['lb_remote'].text = self.pwd
+        sender.title = 'Disconnect'
+      except Exception, e:
+        print e
+    else:
+      if self.smb is not None:
+        del(self.smb);
+      self.share = None
+      self.pwd = ''
+      self.loggedIn = False
+      self.password = None
+      self.username = None
+      sender.title = 'Connect'
+      self.tv_info.text += "\nConnection closed"
+
+  def bt_upload(self, sender):	#put
+    if self.loggedIn:
+      pos = self.localFile.rfind('/')
+      fileName = self.localFile[pos+1:]
+      fh = open(self.localFile, 'rb')
+      if self.pwd == '\\':
+        self.smb.putFile(self.share, self.pwd + fileName, fh.read)
+      else:
+        self.smb.putFile(self.share, self.pwd + '\\' + fileName, fh.read)
+      fh.close()
+      all = self.get_remote_dir()
+      self.refresh_table(self.tv_remote,self.lst_remote,all)
+
+  def bt_download(self, sender):	#get
+    if self.loggedIn:
+      pos = self.remoteFile.rfind('\\')
+      fileName = self.remoteFile[pos+1:]
+      fh = open(self.path + '/' + fileName,'wb')
+      try:
+        self.smb.getFile(self.share, self.remoteFile, fh.write)
+      except:
+        fh.close()
+        os.remove(self.path + fileName)
+      fh.close()
+      all = self.get_dir()
+      self.refresh_table(self.tv_local,self.lst_local,all)
+
+  def table_local_tapped(self, sender):
+    rowtext = sender.items[sender.selected_row]
+    filename_tapped = rowtext
+    if rowtext[0] == '/':
+      if filename_tapped == '/..':
+        pos = self.path.rfind('/')
+        self.path = self.path[:pos]
+      else:
+        self.path = self.path + filename_tapped
+      all = self.get_dir()
+      root_len = len(self.root)
+      self.view['lb_local'].text = self.path[root_len:]
+      self.refresh_table(self.view['tv_local'],self.lst_local,all)
+    else:
+      self.localFile = self.path + '/' + filename_tapped
+
+  def table_remote_tapped(self, sender):
+    rowtext = sender.items[sender.selected_row]
+    filename_tapped = rowtext
+    if rowtext[0] == '\\': #directory
+      if filename_tapped == '\\..': # up
+        pos = self.pwd.rfind('\\')
+        self.pwd = self.pwd[:pos]
+        if self.pwd == '': # root
+          self.pwd = '\\'
+      else:
+        if self.pwd == '\\': # root
+          self.pwd = filename_tapped
+        else:
+          self.pwd += filename_tapped
+      all = self.get_remote_dir()
+      self.refresh_table(self.tv_remote,self.lst_remote,all)
+      self.view['lb_remote'].text = self.pwd
+    else: # file
+      self.remoteFile = self.pwd + '\\' + filename_tapped
+
+  def refresh_table(self, table, lst, data):
+    lst = ui.ListDataSource(data)
+    table.data_source = lst
+    table.delegate = lst
+    table.editing = False
+    lst.font = ('Courier',14)
+    if table.name == 'tv_local':
+      lst.action = self.table_local_tapped
+    else:
+      lst.action = self.table_remote_tapped
+    lst.delete_enabled = False 
+    table.reload_data()
+    return
+
+  def get_dir(self):
+    dirs  = [] if self.path == self.root else ['..']
+    files = []
+    for entry in sorted(os.listdir(self.path)):
+      if os.path.isdir(self.path + '/' + entry):
+        dirs.append(entry)
+      else:
+        files.append(entry)
+    all = ['/' + dir for dir in dirs]
+    for file in files:
+      full_pathname = self.path + '/' + file
+      all.append('{}'.format(file))
+    return all
+
+  def get_remote_dir(self):
+    if self.loggedIn:
+      remoteDir  = [] if self.pwd == '\\' else ['\\..']
+      files = []
+      entries = self.smb.listPath(self.share, self.pwd + '\\*')
+      for e in entries:
+        if e.is_directory() > 0:
+          if str(e.get_longname()) != '.' and str(e.get_longname()) != '..':
+            remoteDir.append('\\' + str(e.get_longname()))
+        else:
+          files.append(str(e.get_longname()))
+      all = sorted(remoteDir)
+      for file in sorted(files):
+        all.append('{}'.format(file))
+      return all
+    else:
+      self.pwd = ''
+      return []
+    
+    #e.get_filesize()
+    #time.ctime(float(e.get_mtime_epoch()))
+
+SMBclient()
